d1faf147aac36b84dcadfe86cd6c883cfd8bf340
Will Pragnell <wpragnell@pivotal.io>
2015-04-16 14:08:14 +0100

    split out info and metrics test

---
+++ info_and_metrics_test.go
@@ -0,0 +1,51 @@
+package garden_acceptance_test
+
+import (
+	"github.com/cloudfoundry-incubator/garden"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("info and metrics", func() {
+	Describe("Container.Info()", func() {
+		It("returns a container IP", func() {
+			container := createContainer(gardenClient, garden.ContainerSpec{Network: "10.1.1.1/16"})
+			info, err := container.Info()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(info.ContainerIP).Should(Equal("10.1.1.1"))
+		})
+	})
+
+	Describe("Client.BulkInfo()", func() {
+		It("returns IPs for multiple containers", func() {
+			container1 := createContainer(gardenClient, garden.ContainerSpec{Network: "10.1.1.1/16"})
+			container2 := createContainer(gardenClient, garden.ContainerSpec{Network: "10.1.1.2/16"})
+			handle1 := container1.Handle()
+			handle2 := container2.Handle()
+
+			infos, err := gardenClient.BulkInfo([]string{handle1, handle2})
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(infos[handle1].Info.ContainerIP).Should(Equal("10.1.1.1"))
+			Ω(infos[handle2].Info.ContainerIP).Should(Equal("10.1.1.2"))
+		})
+	})
+
+	Describe("Container.Metrics()", func() {
+		It("returns the CPU Usage", func() {
+			container := createContainer(gardenClient, garden.ContainerSpec{})
+			metrics, err := container.Metrics()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(metrics.CPUStat.Usage).Should(BeNumerically(">", 0))
+		})
+	})
+
+	Describe("Client.BulkMetrics()", func() {
+		It("returns the CPU Usage (#90241386)", func() {
+			createContainer(gardenClient, garden.ContainerSpec{Handle: "foo"})
+			metricsEntries, err := gardenClient.BulkMetrics([]string{"foo"})
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(metricsEntries["foo"].Metrics.CPUStat.Usage).Should(BeNumerically(">", 0))
+		})
+	})
+})
