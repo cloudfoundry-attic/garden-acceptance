9cae36e23acf4c29c3d2efe031e5763be435fd30
Will Pragnell <wpragnell@pivotal.io>
2015-04-16 12:18:13 +0100

    split out bind_mounts test

---
+++ bind_mounts_test.go
@@ -0,0 +1,67 @@
+package garden_acceptance_test
+
+import (
+	"github.com/cloudfoundry-incubator/garden"
+	"github.com/cloudfoundry-incubator/garden/client"
+	"github.com/cloudfoundry-incubator/garden/client/connection"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("bind_mounts", func() {
+	var gardenClient client.Client
+
+	BeforeEach(func() {
+		gardenClient = client.New(connection.New("tcp", "127.0.0.1:7777"))
+		destroyAllContainers(gardenClient)
+	})
+
+	AfterEach(func() {
+		destroyAllContainers(gardenClient)
+	})
+
+	It("mounts a read-only BindMount (#75464648)", func() {
+		runCommand("sudo rm -f /var/bindmount-test")
+
+		container := createContainer(gardenClient, garden.ContainerSpec{
+			BindMounts: []garden.BindMount{
+				garden.BindMount{
+					SrcPath: "/var",
+					DstPath: "/home/vcap/readonly",
+					Mode:    garden.BindMountModeRO},
+			},
+		})
+
+		runCommand("sudo touch /var/bindmount-test")
+		stdout := runInContainerSuccessfully(container, "ls -l /home/vcap/readonly")
+		Ω(stdout).Should(ContainSubstring("bindmount-test"))
+
+		_, stderr, _ := runInContainer(container, "rm /home/vcap/readonly/bindmount-test")
+		Ω(stderr).Should(ContainSubstring("Read-only file system"))
+
+		runCommand("sudo rm -f /var/bindmount-test")
+	})
+
+	It("mounts a read/write BindMount (#75464648)", func() {
+		container := createContainer(gardenClient, garden.ContainerSpec{
+			BindMounts: []garden.BindMount{
+				garden.BindMount{
+					SrcPath: "/home/vcap",
+					DstPath: "/home/vcap/readwrite",
+					Mode:    garden.BindMountModeRW,
+					Origin:  garden.BindMountOriginContainer,
+				},
+			},
+		})
+
+		stdout := runInContainerSuccessfully(container, "ls -l /home/vcap/readwrite")
+		Ω(stdout).ShouldNot(ContainSubstring("bindmount-test"))
+
+		stdout = runInContainerSuccessfully(container, "touch /home/vcap/readwrite/bindmount-test")
+		stdout = runInContainerSuccessfully(container, "ls -l /home/vcap/readwrite")
+		Ω(stdout).Should(ContainSubstring("bindmount-test"))
+
+		runCommand("sudo rm -f /var/bindmount-test")
+	})
+})
--- garden_acceptance_test.go
+++ garden_acceptance_test.go
@@ -598,49 +598,4 @@ var _ = Describe("Garden Acceptance Tests", func() {
 		})
 	})
 
-	Describe("BindMounts", func() {
-		It("mounts a read-only BindMount (#75464648)", func() {
-			runCommand("sudo rm -f /var/bindmount-test")
-
-			container := createContainer(gardenClient, garden.ContainerSpec{
-				BindMounts: []garden.BindMount{
-					garden.BindMount{
-						SrcPath: "/var",
-						DstPath: "/home/vcap/readonly",
-						Mode:    garden.BindMountModeRO},
-				},
-			})
-
-			runCommand("sudo touch /var/bindmount-test")
-			stdout := runInContainerSuccessfully(container, "ls -l /home/vcap/readonly")
-			Ω(stdout).Should(ContainSubstring("bindmount-test"))
-
-			_, stderr, _ := runInContainer(container, "rm /home/vcap/readonly/bindmount-test")
-			Ω(stderr).Should(ContainSubstring("Read-only file system"))
-
-			runCommand("sudo rm -f /var/bindmount-test")
-		})
-
-		It("mounts a read/write BindMount (#75464648)", func() {
-			container := createContainer(gardenClient, garden.ContainerSpec{
-				BindMounts: []garden.BindMount{
-					garden.BindMount{
-						SrcPath: "/home/vcap",
-						DstPath: "/home/vcap/readwrite",
-						Mode:    garden.BindMountModeRW,
-						Origin:  garden.BindMountOriginContainer,
-					},
-				},
-			})
-
-			stdout := runInContainerSuccessfully(container, "ls -l /home/vcap/readwrite")
-			Ω(stdout).ShouldNot(ContainSubstring("bindmount-test"))
-
-			stdout = runInContainerSuccessfully(container, "touch /home/vcap/readwrite/bindmount-test")
-			stdout = runInContainerSuccessfully(container, "ls -l /home/vcap/readwrite")
-			Ω(stdout).Should(ContainSubstring("bindmount-test"))
-
-			runCommand("sudo rm -f /var/bindmount-test")
-		})
-	})
 })
