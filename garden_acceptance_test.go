2f93cc4aeb14c3473cd32c246cc27514984cac95
Will Pragnell <wpragnell@pivotal.io>
2015-04-16 12:26:05 +0100

    split out server restarting test

--- garden_acceptance_test.go
+++ garden_acceptance_test.go
@@ -306,39 +306,6 @@ var _ = Describe("Garden Acceptance Tests", func() {
 		Ω(stderr).Should(ContainSubstring("No such file or directory"))
 	})
 
-	Describe("When the server is restarted", func() {
-		restartGarden := func() {
-			stdout, _, err := runCommand("sudo /vagrant/vagrant/ctl restart")
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(stdout).Should(ContainSubstring("Starting server"))
-		}
-
-		It("continues to run the containers", func() {
-			// Restarting the Garden server is expensive, so we lump all of the tests into this big It statement.
-			By("Setup containers and restart garden.")
-			container := createContainer(gardenClient, garden.ContainerSpec{Privileged: true})
-			container2 := createContainer(gardenClient, garden.ContainerSpec{Privileged: true})
-			Ω(container.NetOut(pingRule("8.8.8.8"))).ShouldNot(HaveOccurred())
-
-			restartGarden()
-
-			By("Restore multiple privileged containers (#88685840)")
-			_, err := container.Info()
-			Ω(err).ShouldNot(HaveOccurred())
-			_, err = container2.Info()
-			Ω(err).ShouldNot(HaveOccurred())
-
-			By("Run a process in a restored container (#88686146)")
-			_, err = container.Run(lsProcessSpec, silentProcessIO)
-			Ω(err).ShouldNot(HaveOccurred())
-
-			By("NetOut survives restart (#82554270)")
-			stdout := runInContainerSuccessfully(container, "ping -c 1 -w 3 8.8.8.8")
-			Ω(stdout).Should(ContainSubstring("64 bytes from"))
-			Ω(stdout).ShouldNot(ContainSubstring("100% packet loss"))
-		})
-	})
-
 	Describe("Destroy", func() {
 		It("fails when attempting to delete a container twice (#76616270)", func() {
 			container := createContainer(gardenClient, garden.ContainerSpec{})
